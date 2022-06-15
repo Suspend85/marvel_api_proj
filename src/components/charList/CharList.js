04a69664f3338339c4a95fa02618352b885804a0
Dmitrii <melomanko@gmail.com>
2022-06-15 18:08:49 +0700

    CharList Component refactoring to hooks

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,80 +1,92 @@
-import React, { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
-class CharList extends Component {
-	state = {
-		charList: [],
-		loading: true,
-		error: false,
-		newItemLoading: false,
-		offset: 0,
-		charEnded: false,
-		activeChar: '',
-	};
-	myRef = React.createRef();
+const CharList = (props) => {
+	const [charList, setCharList] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [newItemLoading, setNewItemLoading] = useState(true);
+	const [error, setError] = useState(false);
+	const [offset, setOffset] = useState(210);
+	const [charEnded, setCharEnded] = useState(false);
 
-	marvelService = new MarvelService();
+	const marvelService = new MarvelService();
 
-	componentDidMount() {
-		this.onRequest();
-	}
+	useEffect(() => {
+		window.addEventListener('scroll', onScroll);
+		return () => window.removeEventListener('scroll', onScroll);
+	}, []);
 
-	onRequest = (offset) => {
-		this.onCharListLoading();
-		this.marvelService
-			.getAllCharacters(offset) // когда получаем элементы с сервера
-			.then(this.onCharListLoaded) // запускается и получает в себя как аргумент - новый массив с новыми персонажами
-			.catch(this.onError);
-	};
-
-	onCharListLoading = () => {
-		this.setState({
-			newItemLoading: true,
-		});
-	};
-
-	onCharListLoaded = (newCharList) => {
-		let ended = false;
-		if (newCharList.length < 9) {
-			ended = true;
+	useEffect(() => {
+		if (newItemLoading && !charEnded) {
+			onRequest();
 		}
+	}, [newItemLoading]);
 
-		this.setState(({ offset, charList }) => ({
-			charList: [...charList, ...newCharList],
-			loading: false,
-			newItemLoading: false,
-			offset: offset + 9,
-			charEnded: ended,
-		}));
+	const onScroll = () => {
+		if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+			setNewItemLoading(true);
+		}
 	};
 
-	onError = () => {
-		this.setState({
-			error: true,
-			loading: false,
-		});
+	const onRequest = () => {
+		onCharListLoading();
+		marvelService
+			.getAllCharacters(offset)
+			.then(onCharListLoaded)
+			.catch(onError)
+			.finally(() => setNewItemLoading(false));
 	};
 
-	renderItems(arr) {
-		const items = arr.map((item) => {
-			const onActiveChar = () => {
-				this.setState({ activeChar: item.id });
-				this.props.onCharSelected(item.id);
-			};
-			const active = this.state.activeChar === item.id,
-				clazz = active ? 'char__item char__item_selected' : 'char__item';
+	const onCharListLoading = () => {
+		setNewItemLoading(true);
+	};
 
+	const onCharListLoaded = (newCharList) => {
+		setCharList((charList) => [...charList, ...newCharList]);
+		setLoading((loading) => false);
+		setError(false);
+		setOffset((offset) => offset + 9);
+		setCharEnded(newCharList.length < 9 ? true : false);
+	};
+
+	const onError = () => {
+		setError(true);
+		setLoading(false);
+	};
+
+	const itemRefs = useRef([]);
+
+	const focusOnItem = (id, itemId) => {
+		itemRefs.current.forEach((item) => item.classList.remove('char__item_selected'));
+		itemRefs.current[id].classList.add('char__item_selected');
+		itemRefs.current[id].focus();
+	};
+
+	function renderItems(arr) {
+		const items = arr.map((item, i) => {
 			let imgStyle = { objectFit: 'cover' };
 			if (item.thumbnail.includes('image_not_available')) {
 				imgStyle = { objectFit: 'unset' };
 			}
 
 			return (
-				<li className={clazz} key={item.id} onClick={onActiveChar} onFocus={onActiveChar} tabIndex={0}>
+				<li
+					className="char__item"
+					key={item.id}
+					tabIndex={0}
+					ref={(el) => (itemRefs.current[i] = el)}
+					onClick={() => {
+						props.onCharSelected(item.id);
+						focusOnItem(i);
+					}}
+					onFocus={() => {
+						props.onCharSelected(item.id);
+						focusOnItem(i);
+					}}>
 					<img src={item.thumbnail} alt={item.name} style={imgStyle} />
 					<div className="char__name">{item.name}</div>
 				</li>
@@ -84,31 +96,22 @@ class CharList extends Component {
 		return <ul className="char__grid">{items}</ul>;
 	}
 
-	render() {
-		const { charList, loading, error, offset, newItemLoading, charEnded } = this.state;
+	const items = renderItems(charList);
+	const errorMessage = error ? <ErrorMessage /> : null;
+	const spinner = loading ? <Spinner /> : null;
+	const content = !(loading || error) ? items : null;
 
-		const items = this.renderItems(charList);
-
-		const errorMessage = error ? <ErrorMessage /> : null;
-		const spinner = loading ? <Spinner /> : null;
-		const content = !(loading || error) ? items : null;
-
-		return (
-			<div className="char__list">
-				{errorMessage}
-				{spinner}
-				{content}
-				<button
-					className="button button__main button__long"
-					disabled={newItemLoading}
-					style={{ display: charEnded ? 'none' : 'block' }}
-					onClick={() => this.onRequest(offset)}>
-					<div className="inner">load more</div>
-				</button>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="char__list">
+			{errorMessage}
+			{content}
+			{spinner}
+			<button className="button button__main button__long" disabled={newItemLoading} onClick={() => setNewItemLoading(true)}>
+				<div className="inner">{newItemLoading ? 'Loading...' : 'More'}</div>
+			</button>
+		</div>
+	);
+};
 
 CharList.propTypes = {
 	onCharSelected: PropTypes.func.isRequired,
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,15 +1,13 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
 	_apiKey = 'apikey=a3f3e1bb52e9dfbb1dcd77116fe51560';
-	_baseOffset = 0;
+	_baseOffset = 210;
 
 	getResource = async (url) => {
 		let res = await fetch(url);
-
 		if (!res.ok) {
 			throw new Error(`Could not fetch ${url}, status: ${res.status}`);
 		}
-
 		return await res.json();
 	};
 
